5ff7d37bad2dbc461d84d2925b39bbbbf83c41f0
Mohammadshoyebb <141633689+Mohammadshoyebb@users.noreply.github.com>
2024-06-05 11:39:23 +0530

    commit

---
+++ FindCommonCharacters.java
@@ -0,0 +1,62 @@
+/*
+1002. Find Common Characters
+Easy
+Topics
+Companies
+
+Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
+Example 1:
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+
+Example 2:
+
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of lowercase English letters.
+*/
+
+import java.util.*;
+
+public class FindCommonCharacters {
+    public List<String> commonChars(String[] words) {
+        int[] minFreq = new int[26];
+        Arrays.fill(minFreq, Integer.MAX_VALUE);
+
+        for (String word : words) {
+            int[] charCount = new int[26];
+            for (char c : word.toCharArray()) {
+                charCount[c - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = Math.min(minFreq[i], charCount[i]);
+            }
+        }
+
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < minFreq[i]; j++) {
+                result.add(String.valueOf((char) (i + 'a')));
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        FindCommonCharacters solution = new FindCommonCharacters();
+
+        String[] words1 = {"bella", "label", "roller"};
+        System.out.println(solution.commonChars(words1)); // Output: [e, l, l]
+
+        String[] words2 = {"cool", "lock", "cook"};
+        System.out.println(solution.commonChars(words2)); // Output: [c, o]
+    }
+}
+
